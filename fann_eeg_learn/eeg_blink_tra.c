59fdf2743577b3602d5715a817ea655c2a63cbd8
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-01-28 23:30:05 +0100

    eeg_blink_tra removing comments

--- fann_eeg_learn/eeg_blink_tra.c
+++ fann_eeg_learn/eeg_blink_tra.c
@@ -9,7 +9,7 @@ int main()
 	const unsigned int num_input = 3;
 	const unsigned int num_output = 1;
 	const unsigned int num_layers = 3;
-	const unsigned int num_neurons_hidden = 10;
+	const unsigned int num_neurons_hidden = 6;
 	const float desired_error = (const float) 0.001;
 	const unsigned int max_epochs = 500000;
 	const unsigned int epochs_between_reports = 10000;
